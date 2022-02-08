2c8ccc14aa99a83022cbe36f5b50b3c57af3b969
alex baldwin <alex.baldwin@genius.com>
2022-02-08 12:17:48 -0500

    add menu item so pressing the key again loads a new cat, add clickthrough

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 require('dotenv').config();
 
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, shell, BrowserWindow, Menu, MenuItem } = require('electron');
 const fetch = require('make-fetch-happen');
 
-const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=cat`;
+const KEYBOARD_SHORTCUT = 'Cmd+Shift+0';
+const API_URL = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=cat`;
 
 let windw = null;
 
-app.on('ready', () => {
+const template = ({url, permalink}) => `data:text/html;charset=utf-8,
+<!DOCTYPE html>
+<html>
+<body style="margin:0;overflow:hidden;">
+  <a href="${permalink}" target="_blank"><img src="${url}" /></a>
+</body>
+</html>
+`;
 
-  fetch(url).then(response => response.json()).then(async json => {
+const launchCat = async () => {
+  let newWindw;
 
-    const { width, height, url } = json.data.images.original;
+  const response = await fetch(API_URL);
+  const json = await response.json();
 
-    const windowWidth = Number(width);
-    const windowHeight = Number(height);
+  const permalink = json.data.url;
+  const { width, height, url } = json.data.images.original;
 
-    const html = `data:text/html;charset=utf-8,<body style="margin:0;"><img src="${url}" /></body>`;
+  const windowWidth = Number(width);
+  const windowHeight = Number(height);
 
-    windw = new BrowserWindow({
-      width: windowWidth,
-      height: windowHeight,
-      backgroundColor: "#000",
-      useContentSize: true,
-      title: 'keyboard cat',
-      show: false,
-      icon: path.join(__dirname, 'paw.png')
-    });
+  const html = template({url, permalink});
 
-    windw.loadURL(html);
+  newWindw = new BrowserWindow({
+    width: windowWidth,
+    height: windowHeight,
+    backgroundColor: "#000",
+    useContentSize: true,
+    title: 'keyboard cat',
+    show: false,
+    icon: path.join(__dirname, 'paw.png'),
+    useContentSize: true,
+  });
+  newWindw.loadURL(html);
 
-    windw.once('ready-to-show', () => {
-      windw.show();
-    });
-
+  newWindw.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
   });
 
+  newWindw.once('ready-to-show', () => {
+    newWindw.show();
+    if (windw) {
+      windw.close();
+    }
+    windw = newWindw;
+  });
+};
 
+const menu = new Menu();
+menu.append(new MenuItem({
+  label: 'Cat',
+  submenu: [{
+    label: 'New Cat',
+    accelerator: KEYBOARD_SHORTCUT,
+    click: () => {
+      launchCat();
+    }
+  }]
+}));
+
+Menu.setApplicationMenu(menu);
+
+app.on('ready', () => {
+  launchCat();
 });
